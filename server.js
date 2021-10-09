7f15e4c3f70de635ad59f46836a982fd721955ce
Adolf <owner@Users-MacBook-Air.local>
2021-10-08 18:56:02 -0700

    add GraphQL to Express server

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
-
+// const expressGraphQL = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql')
 const app = express();
+
+// Tell the Express app that if any request comes into our app looking for '/graphql',
+// then we want the GraphQL library to handle it.
+// app.use('/graphql', expressGraphQL ({
+//     graphiql: true
+// }));
+app.use('/graphql', graphqlHTTP({
+    graphiql: true
+}))
 
 app.listen(4000, () => {
     console.log('Listening...');
